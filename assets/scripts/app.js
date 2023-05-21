12b4c8044958efca11453a55c2d3595f16ed0469
AnzorNaghawi <anzor.naghawi@realsoft-me.com>
2023-05-21 15:16:29 +0300

    .

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,54 @@
 const bookList = [];
 var itemId = 0;
+var titleval = false;
+var imgval = false;
+var ratingval = false;
 var displayedbooks = [];
-// -----
+var displayedbooksnum = 0;
+const title= document.getElementById("title");
+const imgurl= document.getElementById('image-url');
+const rating= document.getElementById('rating');
+const format =  /[!@#$%^&*()_+\-=\[\]{};'"\|,<>\/?]/g;
+title.addEventListener("change",function(){
+    if(format.test(title.value)){
+        document.getElementById('title-validate').hidden=false;
+        console.log('in true');
+        titleval = false;
+        return;
+    }
+    else
+    {
+        titleval = true;
+        document.getElementById('title-validate').hidden=true;
+    }
+})
+imgurl.addEventListener("change",function(){
+    if(format.test(imgurl   .value)){
+        document.getElementById('image-validate').hidden=false;
+        console.log('in true');
+        imgval = false;
+        return;
+    }
+    else
+    {
+        imgval = true;
+        document.getElementById('image-validate').hidden=true;
+    }
+})
+rating.addEventListener("change",function(){
+    if(!(rating.value <= 5 && rating.value >= 1)){
+        document.getElementById('rating-validate').hidden=false;
+        console.log('in true');
+        ratingval = false;
+        return;
+    }
+    else
+    {
+        ratingval=true;
+        document.getElementById('rating-validate').hidden=true;
+    }
+})
+
 function AddBook(){
     console.log("OPENING DIALOG");
     document.getElementById("backdrop").style.display ="flex";
@@ -11,16 +58,18 @@ function AddBook(){
 
 function AddBook_end(Action){
     // TODO: Clear input on close DONE
-    // READ ABOUT VAT vs LET (Hoisting, Scopes, etc..)
-    // Validate and sanitize inputs
-    var title= document.getElementById("title");
-    var imgurl= document.getElementById('image-url');
-    var rating= document.getElementById('rating');
+    // READ ABOUT VAT vs LET (Hoisting, Scopes, etc..) 
+    // Validate and sanitize inputs Done
     if(Action === 'Ok'){
+        if(!(ratingval && imgval && titleval)){
+            return;
+        }
         console.log("ADDED");
         console.log(title, imgurl, rating);
         generateBookObject(title.value, imgurl.value, rating.value)
         document.getElementById('entry-text').style.display = "none";
+        ratingval = imgval = titleval = false;
+
     }
     title.value='';
     imgurl.value='';
@@ -38,6 +87,7 @@ function generateBookObject(titleValue, urlValue, ratingValue){
 
 function displayBook(){
     bookList.forEach((book) => {
+
         if(!(displayedbooks.includes(book.id))){
             console.log(book.id + "ADDED")
             displayedbooks.push(book.id)
@@ -45,7 +95,7 @@ function displayBook(){
             const li = document.createElement('li');
             ul.appendChild(li);
             li.classList.add('book-element')
-            li.setAttribute("onclick","deletebook(this)")
+            li.setAttribute("onclick","deleteprompt(this)")
             li.setAttribute("id", itemId)
             console.log(bookList)
             li.innerHTML = ("<div class=\"book-element__image\">" +
@@ -53,13 +103,46 @@ function displayBook(){
                             "</div>" +
                             "<div class=\"book-element__info\">" +
                             "<h2>" + book.title + "</h2>" + 
-                            "<p> rating" + book.rating + "/5 </p>" +
+                            "<p>" + book.rating + "/5 </p>" +
                             "</div>")
         }
         else{
             console.log(book.id + "ALREADY EXISTS")
         }
     });
+    paginate();
+}
+
+function paginate(){
+    const ul = document.getElementById('book-list');
+    const pagelist = document.getElementById('pagelist');
+    var pagecount =  Math.ceil(ul.children.length / 3);
+    for (var i = 1; i <= pagecount; i++) {
+        if(pagelist.children.length >= i)
+            continue;
+        var li = document.createElement("li");
+        li.innerHTML = i;
+        li.setAttribute("onclick","showpage(parseInt(this.innerHTML))")
+        pagelist.appendChild(li);
+    }
+    showpage(1);
+}
+
+function showpage(num){
+    var start = (num-1)*3;
+    var end = start + 3;
+    ul = document.getElementById('book-list');
+    for(var i = 0; i < ul.children.length; i ++){
+        ul.children[i].style.display = 'none';
+    }
+    for(var i = start; i < end && i < ul.children.length; i++){
+        ul.children[i].style.display = 'block';
+    }
+    pagelist = document.getElementById('pagelist');
+    for(var i = 0; i < pagelist.children.length; i++){
+        pagelist.children[i].classList = 'inactive';
+    }
+    pagelist.children[num-1].classlist = 'active';
 }
 
 function reDisplayBooks(){
@@ -70,21 +153,40 @@ function reDisplayBooks(){
     displayBook()
 }
 
+function deleteprompt(book){
+    if (document.getElementById('delete-modal').style.display=="none" || document.getElementById('delete-modal').hidden == true){
+        document.getElementById('delete-modal').style.display="block";
+        document.getElementById('delete-modal').hidden = false
+        document.getElementById('promptdeleteyes').id = book.id*-1
+    } 
+    else{
+        document.getElementById('delete-modal').style.display="none";    
+        document.getElementById('delete-modal').hidden = true
+    }
+}
 
 function deletebook(book){
-    book.style.display = "none";
+    document.getElementById('delete-modal').style.display="none";  
     console.log(book.id, book.title, book.rating)
-    bookList.splice(bookList.indexOf(book.id)-1, 1)
+    document.getElementById(book.id*-1).style.display="none";
+    bookList.splice(bookList.indexOf(book.id*-1)-1, 1)
     console.log(bookList)
     if ( bookList.length === 0){
         document.getElementById('entry-text').style.display = "flex";
     }
+    document.getElementById(book.id).id = 'promptdeleteyes';  
+
 }
 
 function ShowDropDown(){
     document.getElementById("DropDown"). hidden = document.getElementById("DropDown"). hidden == false ? true:false;
 }
-
+function ShowDropDown1(){
+    document.getElementById("DropDown1"). hidden = document.getElementById("DropDown"). hidden == false ? true:false;
+}
+function ShowDropDownR(){
+    document.getElementById("DropDownR"). hidden = document.getElementById("DropDown"). hidden == false ? true:false;
+}
 function sort(param){
     switch(param){
         case "title":{
@@ -104,6 +206,32 @@ function sort(param){
             console.log(bookList);
             reDisplayBooks();
             break;
+        }
+    }
+}
+
+function filterR(num){
+    ul = document.getElementById('book-list');
+    for(var i = 0; i < ul.children.length; i ++){
+        console.log(ul.children[i].id)
+        if(bookList[ul.children[i].id-1].rating != num){
+            ul.children[i].style.display = 'none';
+        }
+        else{
+            ul.children[i].style.display = 'block';
+        }
+    }
+}
+function filter(){
+    ul = document.getElementById('book-list');
+    var title=document.getElementById('searchtitleInput').value;
+    for(var i = 0; i < ul.children.length; i ++){
+        console.log(ul.children[i].id)
+        if(bookList[ul.children[i].id-1].title != title){
+            ul.children[i].style.display = 'none';
+        }
+        else{
+            ul.children[i].style.display = 'block';
         }
     }
 }
